38939531f1dc0be70044b2893f69a838f2f5a733
Aymen Bch <commercesite7@gmail.com>
2021-05-18 17:54:07 +0300

    working on bulletlist part3

--- lib/draft_js_to_textspan_flutter.dart
+++ lib/draft_js_to_textspan_flutter.dart
@@ -41,125 +41,135 @@ class DraftJSFlutter extends StatelessWidget {
             blockIndex < draftJsObject.blocks.length;
             blockIndex++) {
           String text = draftJsObject.blocks[blockIndex].text;
-          int textLength = draftJsObject.blocks[blockIndex].text != null
-              ? draftJsObject.blocks[blockIndex].text.runes.length
-              : 0;
+          // int textLength = draftJsObject.blocks[blockIndex].text != null
+          //     ? draftJsObject.blocks[blockIndex].text.runes.length
+          //     : 0;
 
-          for (int textIndex = 0; textIndex < textLength; textIndex++) {
-            print('TextIndex: ' + textIndex.toString());
-            textColor = color;
-            textFontWeight = FontWeight.w400;
-            textFontStyle = FontStyle.normal;
-            decoration = TextDecoration.none;
+          // for (int textIndex = 0; textIndex < textLength; textIndex++) {
+          //   print('TextIndex: ' + textIndex.toString());
+          textColor = color;
+          textFontWeight = FontWeight.w400;
+          textFontStyle = FontStyle.normal;
+          decoration = TextDecoration.none;
 
-            for (int inlineStyleIndex = 0;
-                inlineStyleIndex <
-                    draftJsObject.blocks[blockIndex].inlineStyleRanges.length;
-                inlineStyleIndex++) {
-              if (draftJsObject
-                  .blocks[blockIndex].inlineStyleRanges[inlineStyleIndex]
-                  .contains(textIndex)) {
-                switch (draftJsObject.blocks[blockIndex]
-                    .inlineStyleRanges[inlineStyleIndex].style) {
-                  case "BOLD":
-                    textFontWeight = FontWeight.w700;
-                    break;
-                  case "ITALIC":
-                    textFontStyle = FontStyle.italic;
-                    break;
-                  case "UNDERLINE":
-                    decoration = TextDecoration.underline;
-                    break;
-                  case "STRIKETHROUGH":
-                    decoration = TextDecoration.lineThrough;
-                    break;
-                }
-              }
-            }
-
-            for (int entityRangeIndex = 0;
-                entityRangeIndex <
-                    draftJsObject.blocks[blockIndex].entityRanges.length;
-                entityRangeIndex++) {
-              if (draftJsObject
-                  .blocks[blockIndex].entityRanges[entityRangeIndex]
-                  .contains(textIndex)) {
-                textColor = Colors.blue;
+          for (int inlineStyleIndex = 0;
+              inlineStyleIndex <
+                  draftJsObject.blocks[blockIndex].inlineStyleRanges.length;
+              inlineStyleIndex++) {
+            // if (draftJsObject
+            //     .blocks[blockIndex].inlineStyleRanges[inlineStyleIndex]
+            //     .contains(textIndex)) {
+            switch (draftJsObject
+                .blocks[blockIndex].inlineStyleRanges[inlineStyleIndex].style) {
+              case "BOLD":
+                textFontWeight = FontWeight.w700;
+                break;
+              case "ITALIC":
+                textFontStyle = FontStyle.italic;
+                break;
+              case "UNDERLINE":
                 decoration = TextDecoration.underline;
-                recognizer = TapGestureRecognizer()
-                  ..onTap = () {
-                    String key = draftJsObject
-                        .blocks[blockIndex].entityRanges[entityRangeIndex].key
-                        .toString();
-                    EntityMap entityMap = draftJsObject.entityMap[key];
-                    _launchURL(entityMap.data.url);
-                  };
-              }
-            }
+                break;
+              case "STRIKETHROUGH":
+                decoration = TextDecoration.lineThrough;
+                break;
 
-            // if (draftJsObject.blocks[blockIndex].type ==
-            //         "unordered-list-item" &&
-            //     currentIndex != blockIndex) {
-            //   list.add(
-            //     Row(
-            //       children: [
-            //         Text(
-            //           "• ",
-            //           style: TextStyle(
-            //               fontSize: 15.0,
-            //               color: textColor,
-            //               fontStyle: textFontStyle,
-            //               fontWeight: textFontWeight,
-            //               decoration: decoration),
-            //         ),
-            //         RichText(
-            //           text: TextSpan(
-            //             text: "• ",
-            //             recognizer: recognizer,
-            //             style: TextStyle(
-            //                 fontSize: 15.0,
-            //                 color: textColor,
-            //                 fontStyle: textFontStyle,
-            //                 fontWeight: textFontWeight,
-            //                 decoration: decoration),
-            //           ),
-            //         ),
-            //       ],
-            //     ),
-            //   );
-            //   currentIndex = blockIndex;
-            // }
-            // list.add(
-            //   RichText(
-            //     text: TextSpan(
-            //       text: String.fromCharCode(draftJsObject
-            //           .blocks[blockIndex].text.runes
-            //           .toList()[textIndex]),
-            //       recognizer: recognizer,
-            //       style: TextStyle(
-            //           fontSize: fontSize,
-            //           color: textColor,
-            //           fontStyle: textFontStyle,
-            //           fontWeight: textFontWeight,
-            //           decoration: decoration),
-            //     ),
-            //   ),
-            // );
+              // }
+            }
           }
+
+          for (int entityRangeIndex = 0;
+              entityRangeIndex <
+                  draftJsObject.blocks[blockIndex].entityRanges.length;
+              entityRangeIndex++) {
+            // if (draftJsObject
+            //     .blocks[blockIndex].entityRanges[entityRangeIndex]
+            //     .contains(textIndex)) {
+            textColor = Colors.blue;
+            decoration = TextDecoration.underline;
+            recognizer = TapGestureRecognizer()
+              ..onTap = () {
+                String key = draftJsObject
+                    .blocks[blockIndex].entityRanges[entityRangeIndex].key
+                    .toString();
+                EntityMap entityMap = draftJsObject.entityMap[key];
+                _launchURL(entityMap.data.url);
+              };
+            // }
+          }
+
+          // if (draftJsObject.blocks[blockIndex].type ==
+          //         "unordered-list-item" &&
+          //     currentIndex != blockIndex) {
+          //   list.add(
+          //     Row(
+          //       children: [
+          //         Text(
+          //           "• ",
+          //           style: TextStyle(
+          //               fontSize: 15.0,
+          //               color: textColor,
+          //               fontStyle: textFontStyle,
+          //               fontWeight: textFontWeight,
+          //               decoration: decoration),
+          //         ),
+          //         RichText(
+          //           text: TextSpan(
+          //             text: "• ",
+          //             recognizer: recognizer,
+          //             style: TextStyle(
+          //                 fontSize: 15.0,
+          //                 color: textColor,
+          //                 fontStyle: textFontStyle,
+          //                 fontWeight: textFontWeight,
+          //                 decoration: decoration),
+          //           ),
+          //         ),
+          //       ],
+          //     ),
+          //   );
+          //   currentIndex = blockIndex;
+          // }
+          // list.add(
+          //   RichText(
+          //     text: TextSpan(
+          //       text: String.fromCharCode(draftJsObject
+          //           .blocks[blockIndex].text.runes
+          //           .toList()[textIndex]),
+          //       recognizer: recognizer,
+          //       style: TextStyle(
+          //           fontSize: fontSize,
+          //           color: textColor,
+          //           fontStyle: textFontStyle,
+          //           fontWeight: textFontWeight,
+          //           decoration: decoration),
+          //     ),
+          //   ),
+          // );
+          //}
           if (draftJsObject.blocks[blockIndex].type == "unordered-list-item" &&
               currentIndex != blockIndex) {
             list.add(Row(
               children: [
-                Text(
-                  "• ",
-                  style: TextStyle(
-                      fontSize: 15.0,
-                      color: textColor,
-                      fontStyle: textFontStyle,
-                      fontWeight: textFontWeight,
-                      decoration: decoration),
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    child: Align(
+                      alignment: Alignment.topCenter,
+                      child: Text(
+                        "• ",
+                        style: TextStyle(
+                            fontSize: 15.0,
+                            color: textColor,
+                            fontStyle: textFontStyle,
+                            fontWeight: FontWeight.bold,
+                            decoration: decoration),
+                      ),
+                    ),
+                  ),
                 ),
                 Expanded(
+                  flex: 10,
                   child: RichText(
                     text: TextSpan(
                       text: text,
